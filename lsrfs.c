5cf0c097e089192d0d3fdb2ad8d5e534eeeea422
d0k3 <tore.anon@gmail.com>
2016-11-08 02:09:35 +0100

    Some small cleanups and improvements

--- lsrfs.c
+++ lsrfs.c
@@ -8,7 +8,8 @@ static RomFsLv3Header hdr;
 static u32 DirHashTable[MAX_DIRS];
 static u32 FileHashTable[MAX_FILES];
 
-// validate header by checking sizes
+
+// validate header by checking offsets and sizes
 bool validateLv3Header(RomFsLv3Header* hdr) {
     return ((hdr->size_header == 0x28) &&
         (hdr->offset_dirhash  >= hdr->size_header) &&
@@ -18,7 +19,7 @@ bool validateLv3Header(RomFsLv3Header* hdr) {
         (hdr->offset_filedata >= hdr->offset_filemeta + hdr->size_filemeta));
 }
 
-// hash lvl3 path 
+// hash lvl3 path - this is used to find the first offset in the file / dir hash table
 u32 hashLv3Path(u16* name, u32 name_len, u32 offset_parent) {
     u32 hash = offset_parent ^ 123456789;
     for (u32 i = 0; i < name_len; i++)
@@ -26,14 +27,7 @@ u32 hashLv3Path(u16* name, u32 name_len, u32 offset_parent) {
     return hash;
 }
 
-u32 hashLv3PathByte(const char* name, u32 offset_parent) {
-    u16 wname[256];
-    u32 name_len = strnlen(name, 256);
-    for (name_len = 0; name[name_len]; name_len++)
-        wname[name_len] = name[name_len]; // poor mans UTF-8 -> UTF-16
-    return hashLv3Path(wname, name_len, offset_parent);
-}
-
+// search for lvl3 dir
 u32 seekLv3Dir(const char* path, FILE* fp) {
     char lpath[256];
     u32 offset_parent = 0;
@@ -55,7 +49,8 @@ u32 seekLv3Dir(const char* path, FILE* fp) {
             if ((!fread(&dirmeta, 1, sizeof(RomFsLv3DirMeta), fp)) ||
                 (offset_parent != dirmeta.offset_parent))
                 return (u32) -1; // slim chance of endless loop with broken lvl3 here
-            if ((name_len == dirmeta.name_len) && (memcmp(wname, dirmeta.wname, name_len * 2) == 0))
+            printf("%.*ls / %.*ls", name_len, (wchar_t*) wname, name_len, (wchar_t*) dirmeta.wname);
+            if ((name_len == dirmeta.name_len / 2) && (memcmp(wname, dirmeta.wname, name_len * 2) == 0))
                 break;
             offset = dirmeta.offset_samehash;
             if (offset == (u32) -1) return (u32) -1;
@@ -67,11 +62,15 @@ u32 seekLv3Dir(const char* path, FILE* fp) {
     return offset;
 }
 
+// list lvl3 dir
 bool listLv3Dir(const char* path, FILE* fp) {
     u32 offset_parent = seekLv3Dir(path, fp);
     RomFsLv3DirMeta dirmeta_parent;
     u32 cnt_dir = 0;
     u32 cnt_file = 0;
+    
+    // safety check
+    if (offset_parent == (u32) -1) return false;
     
     printf("\nDIRECTORY: %s\n\n", (*path) ? path : "ROOT");
     // get parent dirmeta
@@ -84,10 +83,7 @@ bool listLv3Dir(const char* path, FILE* fp) {
         fseek(fp, OFFSET_HEADER + hdr.offset_dirmeta + offset, SEEK_SET);
         if (!fread(&dirmeta, 1, sizeof(RomFsLv3DirMeta), fp))
             return false;
-        char name[256] = { 0 };
-        for (u32 i = 0; i < dirmeta.name_len / 2; i++)
-            name[i] = (dirmeta.wname[i] & 0xFF); // poor mans UTF-16 -> UTF-8
-        printf("%2u: %-48.48s [DIR]\n", cnt_dir, name);
+        printf("[%2u] [DIR] %.*ls\n", cnt_dir, dirmeta.name_len / 2, (wchar_t*) dirmeta.wname);
         offset = dirmeta.offset_sibling;
         cnt_dir++;
     }
@@ -97,10 +93,7 @@ bool listLv3Dir(const char* path, FILE* fp) {
         fseek(fp, OFFSET_HEADER + hdr.offset_filemeta + offset, SEEK_SET);
         if (!fread(&filemeta, 1, sizeof(RomFsLv3DirMeta), fp))
             return false;
-        char name[256] = { 0 };
-        for (u32 i = 0; i < filemeta.name_len / 2; i++)
-            name[i] = (filemeta.wname[i] & 0xFF); // poor mans UTF-16 -> UTF-8
-        printf("%2u: %-48.48s [%6u]\n", cnt_file, name, (u32) filemeta.size_data);
+        printf("[%2u] [%6u] %.*ls\n", cnt_file, (u32) filemeta.size_data, filemeta.name_len / 2, (wchar_t*) filemeta.wname);
         offset = filemeta.offset_sibling;
         cnt_file++;
     }
@@ -114,53 +107,53 @@ int main( int argc, char** argv ) {
     const char* path = (argc >= 3) ? argv[2] : "";
     
     if (argc < 2) {
-        printf("error: no romfs given");
+        printf("Usage: %s [romfs file] [path to list]\n [romfs file] has to be decrypted\n [path to list] uses '/' as separators\n", *argv);
         return 1;
     }
     
     fp = fopen(argv[1], "rb");
     if (!fp) {
-        printf("error: cannot open %s", argv[1]);
+        printf("error: cannot open %s\n", argv[1]);
         return 1;
     }
     
     // load header
     fseek(fp, OFFSET_HEADER, SEEK_SET);
     if (fread(&hdr, 1, sizeof(RomFsLv3Header), fp) != sizeof(RomFsLv3Header)) {
-        printf("error: file too small");
+        printf("error: file too small\n");
         return 1;
     }
     
     // validate header
     if (!validateLv3Header(&hdr)) {
-        printf("error: header not recognized");
+        printf("error: header not recognized\n");
         return 1;
     }
     
     // check hash table sizes 
     if ((hdr.size_dirhash > MAX_DIRS * sizeof(u32)) ||
         (hdr.size_filehash > MAX_FILES * sizeof(u32))) {
-        printf("error: max # of files / dirs exceeded");
+        printf("error: max # of files / dirs exceeded\n");
         return 1;
     }
     
     // load dir hash tables
     fseek(fp, OFFSET_HEADER + hdr.offset_dirhash, SEEK_SET);
     if (fread(DirHashTable, 1, hdr.size_dirhash, fp) != hdr.size_dirhash) {
-        printf("error: file too small");
+        printf("error: file too small\n");
         return 1;
     }
     
     // load file hash tables
     fseek(fp, OFFSET_HEADER + hdr.offset_filehash, SEEK_SET);
     if (fread(FileHashTable, 1, hdr.size_filehash, fp) != hdr.size_filehash) {
-        printf("error: file too small");
+        printf("error: file too small\n");
         return 1;
     }
     
     // list dir
     if (!listLv3Dir(path, fp)) {
-        printf("error: failed listing files");
+        printf("error: failed listing files\n");
         return 1;
     }
     
